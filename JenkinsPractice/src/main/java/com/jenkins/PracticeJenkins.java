a478a9fafcbb392b5746f51813ace55cd36e8158
TazinB <99142951+TazinB@users.noreply.github.com>
2023-11-12 14:00:41 -0600

    Add files via upload

---
+++ JenkinsPractice/src/main/java/com/jenkins/PracticeJenkins.java
@@ -0,0 +1,16 @@
+package com.jenkins;
+
+public class PracticeJenkins {
+	
+	public void getJenkins() {
+		System.out.println("This is practice Jenkins");
+		
+	}
+	
+	public static void main(String[] args) {
+		PracticeJenkins obj = new PracticeJenkins();
+		obj.getJenkins();
+	}
+	
+
+}
